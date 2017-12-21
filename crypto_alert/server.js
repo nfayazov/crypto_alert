f7426602540b36eb3543008f3fc4ed6853b3f41a
Nadir Fayazov <nfayazov@gmail.com>
2017-12-20 23:21:02 -0800

    First commit

---
+++ crypto_alert/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+//const react_app = require('./src/App')
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname + '/build/public/index.html'));
+})
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("Listening to port %d", port));
