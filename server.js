41e45abc997a2e69c48b38476a4ef65cbb902d31
Nadir Fayazov <nfayazov@gmail.com>
2017-12-21 23:42:36 -0800

    Twilio

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
+const twilio = require('twilio');
+const dotenv = require('dotenv').load();
 
 const app = express();
-//const react_app = require('./src/App')
+const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const cron = require('cron').CronJob;
 
-app.use(express.static(path.join(__dirname, '/build')));
+//var textJob = new cron('53 22 * * *', function () {
+client.messages.create({ to: '+13475150058', from: process.env.TWILIO_PHONE_NUMBER, body: 'It fucking works!' }, 
+                        function (err, data) {
+                           if (err) console.log(err);
+                           else console.log(data)
+                        });
+//});
 
-app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname + '/build/public/index.html'));
+
+/*app.get('/', (req, res) => {
+   res.send('Hello World');
 })
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log("Listening to port %d", port));
+app.listen(port, () => console.log("Listening to port %d", port));*/
